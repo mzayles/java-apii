2116c1ed9b558e7a95a21370e3247795c16563a6
Mariana Alves <marianasdev@gmail.com>
2026-05-10 21:33:21 -0300

    fix: corrigindo direcionamento

--- CadastroPessoa/Principal.java
+++ CadastroPessoa/Principal.java
@@ -1,51 +1,69 @@
-package pacote;
-import java.util.Arrays;
-import java.util.Random;
+package CadastroPessoa;
 import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Random rd = new Random();
-        
-        System.out.print("Informe o tamanho do vetor: ");
-        int tamanho = sc.nextInt();
+
+        System.out.print("Informe a quantidade de pessoas que deseja cadastrar? ");
+        int quantidade = sc.nextInt();
         sc.nextLine();
-        
-        int vetor[] = new int[tamanho];
-        
-        for(int i = 0; i < vetor.length; i++) {
-            vetor[i] = rd.nextInt(tamanho);
-        }
-        
-        System.out.println("\n------------------- Listando Vetor Desordenado -------------------\n");
-        saida(vetor);
-     
-        System.out.println("\n------------------- Listando Vetor Ordenado -------------------\n");
-        ordenacaoInsercao(vetor);
-        saida(vetor);
-        
-        sc.close();
-    }
-    
-    public static void ordenacaoInsercao(int vetor[]) {
-        int aux;
-        
-        for(int i = 0; i < vetor.length; i++) {
-            aux = vetor[i];
-            int j = i - 1;
-            
-            while(j >= 0 && vetor[j] > aux) {
-                vetor[j + 1] = vetor[j];
-                j--;
+
+        if(quantidade > 5) {
+            System.out.println("Informe a quantidade de ATÉ 5 pessoas, não mais.");
+        } else {
+            Pessoa listaPessoas[] = new Pessoa[quantidade];
+
+            System.out.println("\n------------------- Populando Pessoas -------------------");
+            for(int i = 0; i < listaPessoas.length; i++) {
+                listaPessoas[i] = new Pessoa();
+
+                System.out.printf("\n\t* Pessoa [%d]", i+1);
+                System.out.print("\n\tNome: ");
+                listaPessoas[i].nome = sc.nextLine();
+
+                System.out.print("\tEndereço: ");
+                listaPessoas[i].endereco = sc.nextLine();
+
+                System.out.print("\tTelefone: ");
+                listaPessoas[i].telefone = sc.nextLine();
+
+                System.out.print("\tCidade: ");
+                listaPessoas[i].cidade = sc.nextLine();
+
+                System.out.print("\tEstado: ");
+                listaPessoas[i].estado = sc.nextLine();
+
+                System.out.print("\tCEP: ");
+                listaPessoas[i].CEP = sc.nextLine();
+
+                System.out.print("\tRG: ");
+                listaPessoas[i].RG = sc.nextLine();
+                
+                System.out.print("\tCPF: ");
+                listaPessoas[i].CPF = sc.nextLine();
+
+                System.out.print("\tIdade: ");
+                listaPessoas[i].idade = sc.nextInt();
+
+                sc.nextLine();
             }
-            
-            vetor[j + 1] = aux;
+
+            System.out.println("\n------------------- Listando Pessoas -------------------");
+            for(int i = 0; i < listaPessoas.length; i++) {
+                System.out.printf("\n\t* Pessoa [%d]", i+1);
+                System.out.println("\n\tNome: " + listaPessoas[i].nome);
+                System.out.println("\tEndereço: " + listaPessoas[i].endereco);
+                System.out.println("\tTelefone: " + listaPessoas[i].telefone);
+                System.out.println("\tCidade: " + listaPessoas[i].cidade);
+                System.out.println("\tEstado: " + listaPessoas[i].estado);
+                System.out.println("\tCEP: " + listaPessoas[i].CEP);
+                System.out.println("\tRG: " + listaPessoas[i].RG);
+                System.out.println("\tCPF: " + listaPessoas[i].CPF);
+                System.out.println("\tIdade: " + listaPessoas[i].idade);
+            }
         }
-    }
-    
-    public static void saida(int vetor[]) {
-        System.out.println(Arrays.toString(vetor));
-        System.out.println("\n");
+
+        sc.close();
     }
 }
--- OrdenacaoInsercao/Principal.java
+++ OrdenacaoInsercao/Principal.java
@@ -1,19 +1,51 @@
-package OrdenacaoInsercao;
+package pacote;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        Random rd = new Random();
+        
         System.out.print("Informe o tamanho do vetor: ");
         int tamanho = sc.nextInt();
         sc.nextLine();
-
+        
         int vetor[] = new int[tamanho];
-
+        
         for(int i = 0; i < vetor.length; i++) {
-            System.out.printf("Informe o %dº número inteiro: ", i+1);
-            vetor[i] = sc.nextInt();
+            vetor[i] = rd.nextInt(tamanho);
         }
+        
+        System.out.println("\n------------------- Listando Vetor Desordenado -------------------\n");
+        saida(vetor);
+     
+        System.out.println("\n------------------- Listando Vetor Ordenado -------------------\n");
+        ordenacaoInsercao(vetor);
+        saida(vetor);
+        
+        sc.close();
+    }
+    
+    static void ordenacaoInsercao(int vetor[]) {
+        int aux;
+        
+        for(int i = 0; i < vetor.length; i++) {
+            aux = vetor[i];
+            int j = i - 1;
+            
+            while(j >= 0 && vetor[j] > aux) {
+                vetor[j + 1] = vetor[j];
+                j--;
+            }
+            
+            vetor[j + 1] = aux;
+        }
+    }
+    
+    static void saida(int vetor[]) {
+        System.out.println(Arrays.toString(vetor));
+        System.out.println("\n");
     }
 }
